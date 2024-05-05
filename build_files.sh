c3851023d6d20244cb9b3015d20b677b53e60fd9
VirenderRana <virender.rana0009@gmail.com>
2024-05-05 17:01:13 -0600

    INIT8

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,17 @@
+#!/bin/bash
+
+# Exit script if any command fails
+set -e
+
+# Create a Python virtual environment
+python3 -m venv env
+
+# Activate the virtual environment
+source env/bin/activate
+
+# Install Django and other dependencies from requirements.txt
 pip3 install django
-pip3 install -r requirements.txt 
-python3 manage.py collectstatic
+pip3 install -r requirements.txt
+
+# Run Django collectstatic
+python3 manage.py collectstatic --noinput
